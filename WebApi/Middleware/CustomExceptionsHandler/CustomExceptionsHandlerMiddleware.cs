02e764bdceddda238ac1ac82b0ccef49c1e7d9d6
Maksym Kanupa <maksymkanupa@gmail.com>
2023-06-30 18:29:03 +0300

    Add custom exceptions handler middleware

---
+++ WebApi/Middleware/CustomExceptionsHandler/CustomExceptionsHandlerMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Application.Exceptions;
+using Serilog;
+
+namespace WebApi.Middleware.CustomExceptionsHandler;
+
+public class CustomExceptionsHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CustomExceptionsHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExcepitonAsync(context, exception);
+        }
+    }
+
+    private static Task HandleExcepitonAsync(HttpContext context, Exception exception)
+    {
+        var code = exception switch
+        {
+            NotFoundException => HttpStatusCode.NotFound,
+            UserAlreadyExistsException => HttpStatusCode.Conflict,
+            WrongPasswordException => HttpStatusCode.Unauthorized,
+            _ => HttpStatusCode.InternalServerError
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int) code;
+
+        Log.Error(exception.Message);
+
+        return context.Response.WriteAsync(exception.Message);
+    }
+}
---
+++ WebApi/Middleware/CustomExceptionsHandler/CustomExceptionsHandlerMiddlewareExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Middleware.CustomExceptionsHandler;
+
+public static class CustomExceptionsHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCustomExceptionsHandler(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<CustomExceptionsHandlerMiddleware>();
+    }
+}
