c9b7e3367fc72f023c1b10131c1f92ecbeba9f4e
Maksym Kanupa <maksymkanupa@gmail.com>
2023-06-30 18:27:07 +0300

    Add Dtos

---
+++ Application/Dtos/LoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Dtos;
+
+public class LoginDto
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
---
+++ Application/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using Application.Entities;
+
+namespace Application.Dtos;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string AvatarUrl { get; set; }
+
+    public required List<UserAnime> LastWatchedAnimes { get; set; }
+}
