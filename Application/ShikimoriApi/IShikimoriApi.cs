8b24e4e9cfca584426d2d1d7c5610ced693ef5a0
Maksym Kanupa <maksymkanupa@gmail.com>
2023-06-30 18:25:00 +0300

    Add Shikimori api interface

---
+++ Application/ShikimoriApi/IShikimoriApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using ShikimoriSharp.Classes;
+using ShikimoriSharp.Enums;
+
+namespace Application.ShikimoriApi;
+
+public interface IShikimoriApi
+{
+    [Get("/animes?order={order}&status={status}&limit={limit}&page={page}&kind={kind}&season={season}")]
+    Task<Anime[]> GetAnime(Order order, string status, int limit, int page, string kind, string season);
+}
