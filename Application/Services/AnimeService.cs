7d94a1f10e9d6f91d8fa4beba372065104cbd81a
Maksym Kanupa <maksymkanupa@gmail.com>
2023-06-30 18:26:32 +0300

    Add Anime and User services

---
+++ Application/Services/AnimeService.cs
@@ -0,0 +1,175 @@
+﻿using Application.AnimeSkipApi;
+using Application.AnimeSkipApi.Entities;
+using Application.Entities;
+using Application.KodikApi;
+using Application.KodikApi.Entities;
+using Application.ShikimoriApi;
+using FuzzySharp;
+using Mapster;
+using ShikimoriSharp;
+using ShikimoriSharp.AdditionalRequests;
+using ShikimoriSharp.Enums;
+using ShikimoriSharp.Settings;
+using Anime = Application.Entities.Anime;
+
+namespace Application.Services;
+
+public class AnimeService
+{
+    private readonly ShikimoriClient _shikimoriClient;
+    private readonly IKodikApi _kodikApi;
+    private readonly IShikimoriApi _shikimoriApi;
+    private readonly AnimeSkipService _animeSkipService;
+
+    public AnimeService(ShikimoriClient shikimoriClient, 
+        IKodikApi kodikApi, 
+        IShikimoriApi shikimoriApi, 
+        AnimeSkipService animeSkipService)
+    {
+        _shikimoriClient = shikimoriClient;
+        _kodikApi = kodikApi;
+        _shikimoriApi = shikimoriApi;
+        _animeSkipService = animeSkipService;
+    }
+
+    public async Task<Anime> GetAnimeById(long id)
+    {
+        var shikimoriDetails = await _shikimoriClient.Animes.GetAnime(id);
+        var kodikDetails = await _kodikApi.GetAnime(id);
+        var timestamps = await GetTimestampsById(id);
+
+        // Костыль для Тетради смерти
+        if (id == 1535)
+        {
+            var imageId = shikimoriDetails.Image.Original.Split('?')[1];
+            shikimoriDetails.Image.Original = $"/system/animes/original/1535.jpg?{imageId}";
+        }
+        
+        var anime = new Anime
+        {
+            ShikimoriDetails = shikimoriDetails,
+            KodikDetails = kodikDetails,
+            Timestamps = timestamps?.FindShowsByExternalId.FirstOrDefault()
+        };
+        
+        return anime;
+    }
+
+    private async Task<ShowsByExternalId?> GetTimestampsById(long id)
+    {
+        return await _animeSkipService.GetTimestampsById(id);
+    }
+    
+    public async Task<Franchise> GetFranchise(long id)
+    {
+        var shikimoriDetails = await _shikimoriClient.Animes.GetFranchise(id);
+        
+        return shikimoriDetails;
+    }
+    
+    public async Task<ShikimoriRelated[]> GetRelated(long id)
+    {
+        var shikimoriDetails = await _shikimoriClient.Animes.GetRelated(id);
+        
+        return shikimoriDetails
+            .Where(x => x.Manga is null)
+            .Adapt<ShikimoriRelated[]>();
+    }
+
+    public async Task<ShikimoriAnime[]> GetSimilarAnime(long id)
+    {
+        var similar = await _shikimoriClient.Animes.GetSimilar(id);
+        
+        return similar.Adapt<ShikimoriAnime[]>();
+    }
+
+    public async Task<KodikAnime> SearchAnime(string query)
+    {
+        var search = await _kodikApi.SearchAnime(query);
+
+        // Взять по одному переводу с каждого аниме
+        // Сортировка по совпадению текста + приоритет для "[ТВ-*]"
+        // todo: спарсить JSON со всеми аниме и сделать нормальный поиск
+        var distinctResults = search.Results
+            .DistinctBy(x => x.Shikimori_Id)
+            .OrderByDescending(x => Fuzz.Ratio(query, x.Title) + (x.Title.Contains('[') ? 100 : 0)) 
+            .ThenBy(x => x.Title)
+            .ToList();
+        
+        search.Results = distinctResults;
+
+        foreach (var result in search.Results)
+        {
+            // Костыль, чтоб постеры брались с Шикимори
+            result.Material_Data.Poster_Url = $"https://shikimori.me/system/animes/original/{result.Shikimori_Id}.jpg?1674378220";
+        }
+        
+        return search;
+        // var search = await _shikimoriClient.Animes.GetAnime(new AnimeRequestSettings
+        // {
+        //     order = Order.popularity,
+        //     kind = "tv,movie",
+        //     search = query,
+        //     limit = limit,
+        // });
+        //
+        // var limitAnime = search.Take(limit);
+        //
+        // return limitAnime.Select(x => x.Adapt<AnimePreview>()).ToList();
+    }
+
+    public async Task<List<AnimePreview>> GetPopularAnime(int limit, int page)
+    {
+        var popularAnime = await _shikimoriClient.Animes.GetAnime(new AnimeRequestSettings
+        {
+            order = Order.popularity,
+            limit = limit,
+            page = page,
+            kind = "tv,movie"
+        });
+
+        // Костыль для Тетради смерти
+        if (popularAnime.Any(x => x.Id == 1535))
+        {
+            var deathNote = popularAnime.First(x => x.Id == 1535);
+            var imageId = deathNote.Image.Original.Split('?')[1];
+            deathNote.Image.Original = $"/system/animes/original/1535.jpg?{imageId}";
+        }
+
+        return popularAnime.Select(x => x.Adapt<AnimePreview>()).ToList();
+    }
+    
+    public async Task<List<AnimePreview>> GetTrendingAnime(int limit, int page)
+    {
+        var now = DateTime.Now;
+        var yearAgo = DateTime.Now.AddYears(-1);
+
+        var trendingAnime = await _shikimoriApi.GetAnime(
+            order: Order.popularity,
+            status: "ongoing",
+            limit: limit,
+            page: page,
+            kind: "tv,movie",
+            season: $"{now.Year},{yearAgo.Year}"
+            );
+
+        return trendingAnime.Select(x => x.Adapt<AnimePreview>()).ToList();
+    }
+    
+    public async Task<List<AnimePreview>> GetJustReleasedAnime(int limit, int page)
+    {
+        var now = DateTime.Now;
+        var yearAgo = DateTime.Now.AddYears(-1);
+        
+        var trendingAnime = await _shikimoriApi.GetAnime(
+            order: Order.popularity,
+            status: "released",
+            limit: limit,
+            page: page,
+            kind: "tv,movie",
+            season: $"{now.Year},{yearAgo.Year}"
+            );
+
+        return trendingAnime.Select(x => x.Adapt<AnimePreview>()).ToList();
+    }
+}
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,179 @@
+﻿using Application.Database;
+using Application.Dtos;
+using Application.Entities;
+using Application.Exceptions;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using ShikimoriSharp;
+
+namespace Application.Services;
+
+public class UserService
+{
+    private readonly UserDbContext _dbContext;
+    private readonly ShikimoriClient _shikimoriClient;
+
+    public UserService(UserDbContext dbContext, ShikimoriClient shikimoriClient)
+    {
+        _dbContext = dbContext;
+        _shikimoriClient = shikimoriClient;
+    }
+
+    public async Task<Guid> Register(RegisterDto registerDto)
+    {
+        if (_dbContext.Users.Any(x => x.Username == registerDto.Username))
+        {
+            throw new UserAlreadyExistsException(nameof(registerDto.Username), registerDto.Username);
+        }
+        if (_dbContext.Users.Any(x => x.Email == registerDto.Email))
+        {
+            throw new UserAlreadyExistsException(nameof(registerDto.Email), registerDto.Email);
+        }
+        
+        var user = new User
+        {
+            Email = registerDto.Email,
+            Username = registerDto.Username,
+            Password = registerDto.Password,
+            LastWatchedAnimes = new List<UserAnime>(),
+            AvatarUrl = string.IsNullOrEmpty(registerDto.AvatarUrl) 
+                ? "https://api.dicebear.com/6.x/thumbs/svg" +
+                    $"?seed={registerDto.Username}&shapeColor=43aa52&backgroundColor=daf9d9" 
+                : registerDto.AvatarUrl // todo
+        };
+        _dbContext.Users.Add(user);
+        await _dbContext.SaveChangesAsync();
+
+        return user.Id;
+    }
+
+    public async Task<Guid> Login(LoginDto loginDto)
+    {
+        var user = await _dbContext.Users
+            .FirstOrDefaultAsync(x => x.Username == loginDto.Username);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(loginDto.Username), loginDto.Username);
+        }
+        if (user.Password != loginDto.Password)
+        {
+            throw new WrongPasswordException(loginDto.Username);
+        }
+
+        return user.Id;
+    }
+
+    public async Task<UserDto> GetUser(Guid userId)
+    {
+        var user = await _dbContext.Users
+            .Include(x => x.LastWatchedAnimes)
+            .FirstOrDefaultAsync(x => x.Id == userId);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(userId), userId.ToString());
+        }
+
+        return user.Adapt<UserDto>();
+    }
+    
+    public async Task<UserDto> GetUser(string username)
+    {
+        var user = await _dbContext.Users
+            .Include(x => x.LastWatchedAnimes)
+            .FirstOrDefaultAsync(x => x.Username == username);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(username), username);
+        }
+
+        return user.Adapt<UserDto>();
+    }
+
+    public async Task AddAnimeToWatchingList(Guid userId, long animeId)
+    {
+        var user = _dbContext.Users.Include(x => x.LastWatchedAnimes).FirstOrDefault(x => x.Id == userId);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(userId), userId.ToString());
+        }
+
+        var anime = await _shikimoriClient.Animes.GetAnime(animeId);
+
+        var userAnime = new UserAnime
+        {
+            AnimeId = anime.Id,
+            EpisodesTotal = (int)anime.Episodes,
+            EpisodesWatched = 0,
+            NextEpisode = 1,
+            Title = anime.Russian,
+            PosterUrl = anime.Image.Original,
+            Rating = anime.Score,
+            SecondsWatched = 0
+        };
+        
+        user.LastWatchedAnimes.Add(userAnime);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task FinishEpisode(Guid userId, long animeId, int episodeFinished)
+    {
+        var user = _dbContext.Users.Include(x => x.LastWatchedAnimes).FirstOrDefault(x => x.Id == userId);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(userId), userId.ToString());
+        }
+        
+        var userAnime = user.LastWatchedAnimes.FirstOrDefault(x => x.AnimeId == animeId);
+        if (userAnime is null)
+        {
+            throw new NotFoundException(nameof(animeId), animeId.ToString());
+        }
+
+        if (userAnime.EpisodesTotal == episodeFinished)
+        {
+            await RemoveAnimeFromWatchingList(userId, animeId);
+        }
+        else
+        {
+            userAnime.EpisodesWatched = episodeFinished;
+            userAnime.NextEpisode = episodeFinished + 1;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+    public async Task UpdateTimestamps(Guid userId, long animeId, int secondsWatched)
+    {
+        var user = _dbContext.Users.Include(x => x.LastWatchedAnimes).FirstOrDefault(x => x.Id == userId);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(userId), userId.ToString());
+        }
+        
+        var userAnime = user.LastWatchedAnimes.FirstOrDefault(x => x.AnimeId == animeId);
+        if (userAnime is null)
+        {
+            throw new NotFoundException(nameof(animeId), animeId.ToString());
+        }
+
+        userAnime.SecondsWatched = secondsWatched;
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    public async Task RemoveAnimeFromWatchingList(Guid userId, long animeId)
+    {
+        var user = _dbContext.Users.Include(x => x.LastWatchedAnimes).FirstOrDefault(x => x.Id == userId);
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(userId), userId.ToString());
+        }
+        
+        var userAnime = user.LastWatchedAnimes.FirstOrDefault(x => x.AnimeId == animeId);
+        if (userAnime is null)
+        {
+            throw new NotFoundException(nameof(animeId), animeId.ToString());
+        }
+
+        user.LastWatchedAnimes.Remove(userAnime);
+        await _dbContext.SaveChangesAsync();
+    }
+}
