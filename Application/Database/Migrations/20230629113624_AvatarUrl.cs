6a6e3abb793de2e7bd4997b10a93e7aa5ca9dd8d
Maksym Kanupa <maksymkanupa@gmail.com>
2023-06-30 18:26:44 +0300

    Add SQlite database

---
+++ Application/Database/Migrations/20230629113624_AvatarUrl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AvatarUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "Users");
+        }
+    }
+}
---
+++ Application/Database/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Database;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+    
+    public required DbSet<User> Users { get; set; }
+    public required DbSet<UserAnime> UserAnimes { get; set; }
+}
