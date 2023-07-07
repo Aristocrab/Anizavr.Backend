fcc4ffca96b863d0ec1f5ac85497a6ded2bad9dc
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-07 20:39:08 +0300

    Add request logging middleware

---
+++ WebApi/Middleware/RequestLogging/RequestLoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Serilog;
+
+namespace WebApi.Middleware.RequestLogging;
+
+public class RequestLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestLoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        await _next(context);
+        sw.Stop();
+        var elapsed = sw.Elapsed.TotalMilliseconds;
+        
+        Log.Information("HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.000} ms",
+            context.Request.Method, context.Request.Path, context.Response.StatusCode, elapsed);
+    }
+}
---
+++ WebApi/Middleware/RequestLogging/RequestLoggingMiddlewareExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Middleware.RequestLogging;
+
+public static class RequestLoggingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestLoggingMiddleware>();
+    }
+}
