3a7cf89ffdc71ee0cba04fa77f9e003310a7cd2d
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-07 20:38:06 +0300

    Update DTOs

--- Application/Dtos/RegisterDto.cs
+++ Application/Dtos/RegisterDto.cs
@@ -1,9 +1,11 @@
-﻿namespace Application.Dtos;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Application.Dtos;
 
 public class RegisterDto
 {
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
-    public required string? AvatarUrl { get; set; }
+    public IFormFile? Avatar { get; set; }
 }
