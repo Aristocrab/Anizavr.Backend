b6278191cddc6a9689bb2a783f093844463fe706
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-07 20:37:09 +0300

    Update UserDbContext.cs

--- Application/Database/UserDbContext.cs
+++ Application/Database/UserDbContext.cs
@@ -8,5 +8,16 @@ public class UserDbContext : DbContext
     public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
     
     public required DbSet<User> Users { get; set; }
-    public required DbSet<UserAnime> UserAnimes { get; set; }
+    public required DbSet<UserWatchingAnime> UserWatchingAnimeList { get; set; }
+    public required DbSet<UserWatchedAnime> UserWatchedAnimeList { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<User>()
+            .Property(e => e.Email)
+            .HasConversion(
+                convertToProviderExpression: email => email.Value,
+                convertFromProviderExpression: email => Email.From(email));
+    }
 }
