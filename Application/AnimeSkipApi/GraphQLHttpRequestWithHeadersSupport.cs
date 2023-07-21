337a02cd9cc50dbadd481253de28da42242174b5
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-21 11:28:12 +0300

    Update GraphQLHttpRequestWithHeadersSupport.cs

--- Application/AnimeSkipApi/GraphQLHttpRequestWithHeadersSupport.cs
+++ Application/AnimeSkipApi/GraphQLHttpRequestWithHeadersSupport.cs
@@ -9,7 +9,7 @@ public class GraphQlHttpRequestWithHeadersSupport : GraphQLHttpRequest
     public override HttpRequestMessage ToHttpRequestMessage(GraphQLHttpClientOptions options, IGraphQLJsonSerializer serializer)
     {
       var requestMessage = base.ToHttpRequestMessage(options, serializer);
-      requestMessage.Headers.Add("x-client-id", Constants.AnimeSkipKey); 
+      requestMessage.Headers.Add("x-client-id", Constants.AnimeSkipKey); // todo 
       return requestMessage;
     }
 }
