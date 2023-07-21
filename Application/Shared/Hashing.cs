bbfe1aee5ae4722a1f359071e7f3fbc9a7831e2a
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-21 11:32:10 +0300

    Create Hashing.cs

---
+++ Application/Shared/Hashing.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Application.Shared;
+
+public static class Hashing
+{
+    public static (string hash, string salt) HashPassword(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(128 / 8);
+
+        var passwordHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return (passwordHash, Convert.ToBase64String(salt));
+    }
+    
+    public static string HashPassword(string password, string salt)
+    {
+        var saltConverted = Convert.FromBase64String(salt);
+
+        var passwordHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: saltConverted,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return passwordHash;
+    }
+}
