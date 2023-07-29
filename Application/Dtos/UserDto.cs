3c5447ad93db452267e9027218b30da9dba48de7
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-29 11:44:24 +0300

    Update UserDto.cs

--- Application/Dtos/UserDto.cs
+++ Application/Dtos/UserDto.cs
@@ -6,10 +6,11 @@ public class UserDto
 {
     public Guid Id { get; set; }
     public required string Username { get; set; }
-    public required string Email { get; set; }
+    // public required string Email { get; set; }
     public required string AvatarUrl { get; set; }
 
     public required List<UserWatchingAnime> CurrentlyWatchingAnime { get; set; }
     public required List<UserWatchedAnime> WatchedAnime { get; set; }
     public required List<WishlistAnime> Wishlist { get; set; }
+    public required List<TierlistAnime> Tierlist { get; set; }
 }
