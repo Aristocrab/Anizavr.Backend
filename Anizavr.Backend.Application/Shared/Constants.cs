7a871df0c7bdb27c0372518146d4753dda699b2c
Maksym Kanupa <maksymkanupa@gmail.com>
2023-07-31 13:21:04 +0300

    Add Constants

---
+++ Anizavr.Backend.Application/Shared/Constants.cs
@@ -0,0 +1,25 @@
+﻿namespace Anizavr.Backend.Application.Shared;
+
+public class Constants
+{
+    public const string Issuer = "https://anizavr.me";
+    public const string Audience = "https://anizavr.me";
+    public static readonly string JwtSecretKey = GetEnvironmentVariable("ANIZAVR_JwtSecretKey");
+
+    public const string ShikimoriClientName = "AnimeWiki4All";
+    public const string ShikimoriClientId = "jsnkSxyHQ4g5SiB9XXEBIF6akkHonGKNioQTsWEBxh4";
+    public static readonly string ShikimoriClientKey = GetEnvironmentVariable("ANIZAVR_ShikimoriClientKey");
+    
+    public static readonly string AnimeSkipKey = GetEnvironmentVariable("ANIZAVR_AnimeSkipKey");
+    
+    public static readonly string KodikKey = GetEnvironmentVariable("ANIZAVR_KodikKey");
+    
+    private static string GetEnvironmentVariable(string name)
+    {
+        var value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User)
+                    ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process)
+                    ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine);
+
+        return value ?? throw new Exception($"Environment variable {name} is not set");
+    }
+}
