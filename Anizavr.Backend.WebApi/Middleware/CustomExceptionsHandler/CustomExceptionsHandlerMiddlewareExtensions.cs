cd628b79172f40d373e774a0a269b180de5569b9
Maksym Kanupa <maksymkanupa@gmail.com>
2023-08-21 16:13:38 +0300

    Remove middleware

--- Anizavr.Backend.WebApi/Middleware/CustomExceptionsHandler/CustomExceptionsHandlerMiddlewareExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Anizavr.Backend.WebApi.Middleware.CustomExceptionsHandler;
-
-public static class CustomExceptionsHandlerMiddlewareExtensions
-{
-    public static IApplicationBuilder UseCustomExceptionsHandler(this IApplicationBuilder builder)
-    {
-        return builder.UseMiddleware<CustomExceptionsHandlerMiddleware>();
-    }
-}
