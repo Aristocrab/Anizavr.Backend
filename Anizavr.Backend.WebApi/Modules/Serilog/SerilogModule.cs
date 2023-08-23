7288045715fa9b7321af404858ac8fc97b555da9
Maksym Kanupa <maksymkanupa@gmail.com>
2023-08-23 15:12:15 +0300

    Change logger configuration

--- Anizavr.Backend.WebApi/Modules/Serilog/SerilogModule.cs
+++ Anizavr.Backend.WebApi/Modules/Serilog/SerilogModule.cs
@@ -8,13 +8,13 @@ public class SerilogModule : AppModule
 {
     public override void ConfigureServices(WebApplicationBuilder builder)
     {
-        var formatWithNamespace = "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}";
-        
         Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
             .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
             .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
-            .WriteTo.Console(outputTemplate: formatWithNamespace)
+#if DEBUG
+            .MinimumLevel.Override("Aristocrab.AppModules.AppModule", LogEventLevel.Debug)
+#endif
+            .WriteTo.Console()
             .CreateLogger();
         
         builder.Host.UseSerilog();
