5bd370897a9606f710c290b01291e549f6184b3e
Maksym Kanupa <maksymkanupa@gmail.com>
2023-09-10 17:49:12 +0300

    Update Program.cs

--- Anizavr.Backend.WebApi/Program.cs
+++ Anizavr.Backend.WebApi/Program.cs
@@ -1,11 +1,9 @@
 using AspNetCore.Extensions.AppModules;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.AddModules();
 
 var app = builder.Build();
-
 app.UseModules();
 
 app.Run();
