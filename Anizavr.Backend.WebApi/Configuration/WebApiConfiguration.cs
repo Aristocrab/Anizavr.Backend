842515781d840ea3cadd2515ebfa23933c2dff5e
Maksym Kanupa <maksymkanupa@gmail.com>
2023-09-11 11:33:59 +0300

    Update WebApiConfiguration.cs

--- Anizavr.Backend.WebApi/Configuration/WebApiConfiguration.cs
+++ Anizavr.Backend.WebApi/Configuration/WebApiConfiguration.cs
@@ -2,13 +2,13 @@
 
 public class WebApiConfiguration : IWebApiConfiguration
 {
-    public required string JwtIssuer { get; set; }
-    public required string JwtAudience { get; set; }
-    public required string JwtSecretKey { get; set; }
-    public required string DatabasePath { get; set; }
-    public required string ConnectionString { get; set; }
-    public required string ShikimoriClientName { get; set; }
-    public required string ShikimoriClientId { get; set; }
-    public required string ShikimoriClientKey { get; set; }
-    public required string KodikKey { get; set; }
+    public required string JwtIssuer { get; init; }
+    public required string JwtAudience { get; init; }
+    public required string JwtSecretKey { get; init; }
+    public required string DatabasePath { get; init; }
+    public required string ConnectionString { get; init; }
+    public required string ShikimoriClientName { get; init; }
+    public required string ShikimoriClientId { get; init; }
+    public required string ShikimoriClientKey { get; init; }
+    public required string KodikKey { get; init; }
 }
